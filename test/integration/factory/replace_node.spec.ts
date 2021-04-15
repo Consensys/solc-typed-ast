7843990cb7f8abd6c8c41cfaf77acfb7a44f5b83
blitz-1306 <pavel@maddevs.io>
2021-04-15 16:13:00 +0500

    Restored previous set of test samples for replace_node.spec.ts

--- test/integration/factory/replace_node.spec.ts
+++ test/integration/factory/replace_node.spec.ts
@@ -18,6 +18,50 @@ import {
 } from "../../../src";
 
 const cases: string[] = [
+    "test/samples/solidity/declarations/contract_0413.sol",
+    "test/samples/solidity/declarations/contract_050.sol",
+    "test/samples/solidity/declarations/interface_060.sol",
+
+    "test/samples/solidity/expressions/assignment.sol",
+    "test/samples/solidity/expressions/binary_operation_0413.sol",
+    "test/samples/solidity/expressions/binary_operation_050.sol",
+    "test/samples/solidity/expressions/conditional_0413.sol",
+    "test/samples/solidity/expressions/conditional_050.sol",
+    "test/samples/solidity/expressions/tuple.sol",
+    "test/samples/solidity/expressions/unary_operation_0413.sol",
+    "test/samples/solidity/expressions/unary_operation_050.sol",
+
+    "test/samples/solidity/meta/pragma.sol",
+    "test/samples/solidity/meta/three_simple_contracts.sol",
+    "test/samples/solidity/meta/using_for.sol",
+
+    "test/samples/solidity/statements/block_0413.sol",
+    "test/samples/solidity/statements/block_050.sol",
+    "test/samples/solidity/statements/do_while_0413.sol",
+    "test/samples/solidity/statements/do_while_050.sol",
+    "test/samples/solidity/statements/emit_0421.sol",
+    "test/samples/solidity/statements/emit_050.sol",
+    "test/samples/solidity/statements/expression_0413.sol",
+    "test/samples/solidity/statements/expression_050.sol",
+    "test/samples/solidity/statements/for_0413.sol",
+    "test/samples/solidity/statements/for_050.sol",
+    "test/samples/solidity/statements/if_0413.sol",
+    "test/samples/solidity/statements/if_050.sol",
+    "test/samples/solidity/statements/inline_assembly_0413.sol",
+    "test/samples/solidity/statements/inline_assembly_050.sol",
+    "test/samples/solidity/statements/inline_assembly_060.sol",
+    "test/samples/solidity/statements/placeholder_0413.sol",
+    "test/samples/solidity/statements/placeholder_050.sol",
+    "test/samples/solidity/statements/return_0413.sol",
+    "test/samples/solidity/statements/return_050.sol",
+    "test/samples/solidity/statements/throw_0413.sol",
+    "test/samples/solidity/statements/variable_declaration_0413.sol",
+    "test/samples/solidity/statements/variable_declaration_050.sol",
+    "test/samples/solidity/statements/while_0413.sol",
+    "test/samples/solidity/statements/while_050.sol",
+
+    "test/samples/solidity/types/types.sol",
+
     "test/samples/solidity/latest_06.sol",
     "test/samples/solidity/latest_07.sol",
     "test/samples/solidity/latest_08.sol"
