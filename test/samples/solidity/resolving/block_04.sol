68880dd5911e74b0274cb3894eca290a7d14e77a
d1ll0n <>
2022-11-08 02:16:37 -0600

    Extend tests to validate block scope resolution

--- test/samples/solidity/resolving/block_04.sol
+++ test/samples/solidity/resolving/block_04.sol
@@ -8,5 +8,27 @@ contract Foo {
         foo memory m = foo(bar);
         
         uint bar = m.x;
+
+        uint m1 = k;
+        {uint k;}
+    }
+
+    uint256 x1;
+    uint256 x2;
+    uint256 x3;
+    uint256 x4;
+
+    function second() internal {
+        {
+            uint256 x1 = 1;
+            if (x1 > 0) {
+                uint256 x2 = 2;
+            }
+            {
+                uint256 x3 = 3;
+            }
+        }
+        for (uint256 x4; x4 < 10; x4++) {}
+        uint256 y = x1 + x2 + x3 + x4;
     }
 }
