4b422ca834871cf06d287535048b017b2bf7ad0e
blitz-1306 <hyperx27@gmail.com>
2023-05-31 10:55:54 +0500

    Fix test due to contract constructor signature change

--- test/unit/types/abi_encodable.spec.ts
+++ test/unit/types/abi_encodable.spec.ts
@@ -132,6 +132,7 @@ const cases: Array<
                 false,
                 true,
                 [],
+                [],
                 []
             );
 
@@ -292,6 +293,7 @@ const cases: Array<
                 ContractKind.Contract,
                 false,
                 true,
+                [],
                 [],
                 []
             );
